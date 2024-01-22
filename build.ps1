f6c59439fd9417d532ac6082683303740ef90783
jonteohr <jonathan.ohrstrom@saabgroup.com>
2024-01-22 15:06:20 +0100

    Test script

--- build.ps1
+++ build.ps1
@@ -15,7 +15,7 @@ if($workspace) {
 Write-Output "Parsing control-file: $control_file"
 foreach($line in Get-Content $control_file) {
 	$command = ($line -split ' ')[0]
-	if($command -eq "#") return # It's a comment so let's skip it!
+	if($command -eq "#") {return} # It's a comment so let's skip it!
 	
 	Write-Output "Found command: $command"
 }
